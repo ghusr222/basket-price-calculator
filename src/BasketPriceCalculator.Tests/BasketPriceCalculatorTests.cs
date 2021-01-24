58801171c427968baccda043c4dd010f22f11473
driznar <driznar@gmail.com>
2021-01-24 20:34:34 +0000

    Adding 2nd failing test (no discount)

--- src/BasketPriceCalculator.Tests/BasketPriceCalculatorTests.cs
+++ src/BasketPriceCalculator.Tests/BasketPriceCalculatorTests.cs
@@ -27,15 +27,30 @@ namespace BasketPriceCalculator.Tests
         {
             var basketItemList = new List<BasketItem>
             {
-                new BasketItem { Product=new Product{ Name="Bread", Price=0.8m }, Quantity=1 },
+                new BasketItem { Product=new Product{ Name="Bread", Price=1.0m }, Quantity=1 },
                 new BasketItem { Product=new Product{ Name="Milk", Price=1.15m }, Quantity=1 },
-                new BasketItem { Product=new Product{ Name="Butter", Price=1.0m }, Quantity=1 }
+                new BasketItem { Product=new Product{ Name="Butter", Price=0.8m }, Quantity=1 }
             };
 
             var result = _sut.Calculate(basketItemList);
 
             Assert.AreEqual(2.95m, result);
         }
+
+        [Test]
+        public void Calculate_OneBreadOneMilk_ReturnsValue()
+        {
+            var basketItemList = new List<BasketItem>
+            {
+                new BasketItem { Product=new Product{ Name="Bread", Price=0.8m }, Quantity=1 },
+                new BasketItem { Product=new Product{ Name="Milk", Price=1.15m }, Quantity=1 },
+            };
+
+            var result = _sut.Calculate(basketItemList);
+
+            Assert.AreEqual(1.95m, result);
+        }
+
 
     }
 }
